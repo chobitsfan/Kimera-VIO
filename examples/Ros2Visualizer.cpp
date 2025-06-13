ebecf93f997567a0c1351b3919a4d6a79fffea1e
chobits tai <mrchobits@gmail.com>
2025-06-13 21:04:30 +0800

    log total latency

--- examples/Ros2Visualizer.cpp
+++ examples/Ros2Visualizer.cpp
@@ -5,6 +5,10 @@ Ros2Visualizer::Ros2Visualizer(std::shared_ptr<KimeraRos2Node> ros2_node) : Visu
 
 VIO::VisualizerOutput::UniquePtr Ros2Visualizer::spinOnce(const VIO::VisualizerInput& viz_input) {
     if (viz_input.backend_output_) {
+        int64_t now_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+        int latency_ms = (now_ns - viz_input.backend_output_->timestamp_) / 1000000;
+        VIO::utils::StatsCollector timing_stats("latency [ms]");
+        timing_stats.AddSample(latency_ms);
         const gtsam::Pose3& pose = viz_input.backend_output_->W_State_Blkf_.pose_;
         const gtsam::Rot3& rotation = pose.rotation();
         const gtsam::Quaternion& quaternion = rotation.toQuaternion();
