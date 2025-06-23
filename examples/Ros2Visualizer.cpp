7b76105e1e31afb7199246f918e7cfdf5689f5ee
chobits tai <mrchobits@gmail.com>
2025-06-23 18:14:17 +0800

    odometry timestamp when image captured

--- examples/Ros2Visualizer.cpp
+++ examples/Ros2Visualizer.cpp
@@ -14,7 +14,8 @@ VIO::VisualizerOutput::UniquePtr Ros2Visualizer::spinOnce(const VIO::VisualizerI
         const gtsam::Quaternion& quaternion = rotation.toQuaternion();
         const gtsam::Vector3& velocity = viz_input.backend_output_->W_State_Blkf_.velocity_;
         nav_msgs::msg::Odometry odo_msg;
-        odo_msg.header.stamp = ros2_node_->get_clock()->now();
+        odo_msg.header.stamp.sec = static_cast<int32_t>(viz_input.backend_output_->timestamp_ / 1000000000);
+        odo_msg.header.stamp.nanosec = static_cast<uint32_t>(viz_input.backend_output_->timestamp_ % 1000000000);
         odo_msg.header.frame_id = "map";
         odo_msg.child_frame_id = "map";
         odo_msg.pose.pose.position.x = -pose.z();
@@ -24,6 +25,9 @@ VIO::VisualizerOutput::UniquePtr Ros2Visualizer::spinOnce(const VIO::VisualizerI
         odo_msg.pose.pose.orientation.y = -quaternion.x();
         odo_msg.pose.pose.orientation.z = quaternion.y();
         odo_msg.pose.pose.orientation.w = quaternion.w();
+        odo_msg.twist.twist.linear.x = -velocity(2);
+        odo_msg.twist.twist.linear.y = -velocity(0);
+        odo_msg.twist.twist.linear.z = velocity(1);
         ros2_node_->odo_pub->publish(odo_msg);
     }
 
