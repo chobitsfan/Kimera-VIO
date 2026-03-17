3506fe677248b9d6b78fcc0ab6600c665f2808be
chobits tai <chobits@itri.org.tw>
2026-03-17 16:45:01 +0800

    fix velocity covariance

--- examples/Ros2Visualizer.cpp
+++ examples/Ros2Visualizer.cpp
@@ -15,27 +15,13 @@ VIO::VisualizerOutput::UniquePtr Ros2Visualizer::spinOnce(const VIO::VisualizerI
         const gtsam::Rot3& rotation = pose.rotation();
         const gtsam::Quaternion& quaternion = rotation.toQuaternion();
         const gtsam::Vector3& velocity = viz_input.backend_output_->W_State_Blkf_.velocity_;
-        const gtsam::Matrix& pose_covariance_lkf = viz_input.backend_output_->pose_covariance_lkf_;
+        const gtsam::Matrix& pose_cov = viz_input.backend_output_->pose_covariance_lkf_;
         gtsam::Matrix pose_cov_ros2(6, 6);
-        pose_cov_ros2.block<3, 3>(0, 0) = pose_covariance_lkf.block<3, 3>(3, 3);  // trans var
-        pose_cov_ros2.block<3, 3>(0, 3) = pose_covariance_lkf.block<3, 3>(3, 0);  // trans-rot cross
-        pose_cov_ros2.block<3, 3>(3, 0) = pose_covariance_lkf.block<3, 3>(0, 3);  // rot-trans cross
-        pose_cov_ros2.block<3, 3>(3, 3) = pose_covariance_lkf.block<3, 3>(0, 0);  // rot var
-        std::array<double, 36> pose_cov;
-        int idx = 0;
-        for (int i = 0; i < 6; i++) {
-          for (int j = 0; j < 6; j++) {
-            pose_cov[idx++] = pose_cov_ros2(i, j);
-          }
-        }
-        gtsam::Matrix vel_cov = viz_input.backend_output_->vel_covariance_lkf_;
-        std::array<double, 36> twist_cov;
-        idx = 0;
-        for (int i = 0; i < 3; i++) {
-          for (int j = 0; j < 3; j++) {
-            twist_cov[idx++] = vel_cov(i, j);
-          }
-        }
+        pose_cov_ros2.block<3, 3>(0, 0) = pose_cov.block<3, 3>(3, 3);  // trans var
+        pose_cov_ros2.block<3, 3>(0, 3) = pose_cov.block<3, 3>(3, 0);  // trans-rot cross
+        pose_cov_ros2.block<3, 3>(3, 0) = pose_cov.block<3, 3>(0, 3);  // rot-trans cross
+        pose_cov_ros2.block<3, 3>(3, 3) = pose_cov.block<3, 3>(0, 0);  // rot var
+        const gtsam::Matrix& vel_cov = viz_input.backend_output_->vel_covariance_lkf_;
         nav_msgs::msg::Odometry odo_msg;
         odo_msg.header.stamp.sec = static_cast<int32_t>(viz_input.backend_output_->timestamp_ / 1000000000);
         odo_msg.header.stamp.nanosec = static_cast<uint32_t>(viz_input.backend_output_->timestamp_ % 1000000000);
@@ -48,11 +34,14 @@ VIO::VisualizerOutput::UniquePtr Ros2Visualizer::spinOnce(const VIO::VisualizerI
         odo_msg.pose.pose.orientation.y = -quaternion.x();
         odo_msg.pose.pose.orientation.z = quaternion.y();
         odo_msg.pose.pose.orientation.w = quaternion.w();
-        odo_msg.pose.covariance = pose_cov;
+        Eigen::Map<Eigen::Matrix<double, 6, 6, Eigen::RowMajor>>(odo_msg.pose.covariance.data()) = pose_cov_ros2;
         odo_msg.twist.twist.linear.x = -velocity(2);
         odo_msg.twist.twist.linear.y = -velocity(0);
         odo_msg.twist.twist.linear.z = velocity(1);
-        odo_msg.twist.covariance = twist_cov;
+        odo_msg.twist.covariance.fill(0);
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                odo_msg.twist.covariance[i * 6 + j] = vel_cov(i, j);
         ros2_node_->odo_pub->publish(odo_msg);
     }
 
