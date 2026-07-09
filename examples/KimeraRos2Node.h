b3dbcda8ac5152c9058d430c367e01595f62ab60
chobits tai <chobits@itri.org.tw>
2026-07-09 08:42:41 +0800

    Revert "use histogram equalization to increase contrast"
    This reverts commit 8807760200a91fdff4a778d9b6cfa261f7687e04.

--- examples/KimeraRos2Node.h
+++ examples/KimeraRos2Node.h
@@ -25,6 +25,4 @@ class KimeraRos2Node : public rclcpp::Node {
         VIO::Pipeline::Ptr vio_pipeline_;
         VIO::VioParams vio_params_;
         uint64_t frame_count_;
-        cv::Ptr<cv::CLAHE> clahe_;
-        cv::Mat clahe_dst_;
 };
